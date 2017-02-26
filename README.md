0b757bff6b9e29d2be37d992276f96ab8f0411cf
mattcarlotta <carlotta.matt@gmail.com>
2017-02-25 21:24:25 -0800

    Refactor entire script

--- README.md
+++ README.md
@@ -1,10 +1,12 @@
-#AppleIntelInfoLoader v0.0.2
+#AppleIntelInfoLoader v1.0.0
 
 Introduction
 
-AppleIntelInfo Loader is a simple automated bash script that loads Piker-Alpha's <a href="https://github.com/Piker-Alpha/AppleIntelInfo">AppleIntelInfo.kext</a> without any user input.
+AppleIntelInfo Loader is a simple automated bash script that can compile and load Piker-Alpha's <a href="https://github.com/Piker-Alpha/AppleIntelInfo">AppleIntelInfo.kext</a> without any user input.
 
-Simply double click the aiiLoader.command file to load the kext.
+Simply double click the aiiLoader.command file to load the script.
+
+Note: If at any point you wish to abort the script, simple press "ctrl+c"!
 
 --------------------------------------------------------------------------------------------------------------
-**Note: This assumes you've already compiled the AppleIntelInfo.kext and placed it on your Desktop.
+**Special Note: Huge thanks to PMHeart for refactoring my entire script and making it more flexible!
--- src/aiiLoader.sh
+++ src/aiiLoader.sh
@@ -1,108 +1,196 @@
 #!/bin/bash
 #
-# AppleIntelInfo Loader Version 0.0.2 - Copyright (c) 2017 by M.F.C.
+# AppleIntelInfo Loader Version 1.0. - Copyright (c) 2017 by M.F.C. and PMHeart
+#
+#
 #
 # Introduction:
 #     - AppleIntelInfo Loader is a simple automated bash script that
 #       loads Piker-Alpha's AppleIntelInfo.kext without any user input.
-#     - Simply unzip the aiiLoader.command file and double click!
+#     - Simply double click the aiiLoader.command file to load the script! 
 #
 #
 # Bugs:
-#			- Bug reports can be filed at:
+#     - Bug reports can be filed at:
 #        https://github.com/mattcarlotta/AppleIntelInfoLoader/issues
-#			- Please provide clear steps to reproduce the bug and the output
+#     - Please provide clear steps to reproduce the bug and the output
 #        of the script. Thank you!
+#
+#
+
 
 #===============================================================================##
 ## GLOBAL VARIABLES #
 ##==============================================================================##
-gFile="AppleIntelInfo.kext"
+
+
+gRepo="$(cd "$(dirname "${BASH_SOURCE[0]}")" && pwd)"
+
+#
+# Kext path, will be overrided by _find_kext()
+#
+gKext=""
+
+gAii="AppleIntelInfo.kext"
+
+gPikerAII="https://github.com/Piker-Alpha/AppleIntelInfo"
 
 
 #===============================================================================##
-## END SCRIPT #
+## CHECK SIP #
 ##==============================================================================##
-function endScript() {
-  exit=$1
 
-  echo ''
-  echo 'Terminating the script...'
-  sleep 1.5
-  if [[ $exit -eq 0 ]];
+
+function _getSIPStat()
+{
+  case "$(/usr/bin/csrutil status)" in
+    "System Integrity Protection status: enabled." )
+      printf 'SIP enabled, aborting...\n'
+      exit 1
+      ;;
+
+    *"Kext Signing: enabled"* )
+      printf 'SIP Kext Restriction enabled, aborting...\n'
+      exit 1
+      ;;
+
+    * )
+      ;;
+  esac
+}
+
+
+#===============================================================================##
+## COMPILE KEXT #
+##==============================================================================##
+
+
+function _find_kext()
+{
+  local dkpKext="$HOME/Desktop/${gAii}"
+
+  #
+  # If the kext cannot be found at current dir,
+  # then we shall search at Desktop.
+  # If still not found,
+  # then we need to download and then compile it.
+  #
+  if [[ ! -e "${gRepo}/${gAii}" ]];
     then
-    #
-    # exit script by terminating window
-    #
-    killall Terminal
+      printf "${gAii} could not be located!\n"
+      printf " \n"
+      printf "Searching for ${gAii} in $HOME/Desktop...\n"
+      sleep 1
+      if [[ -e "${dkpKext}" ]];
+        then
+          printf "${gAii} was found!\n"
+          printf " \n"
+          gKext="${dkpKext}"
+        else
+          printf "${gAii} wasn't found on the Desktop either!\n"
+          printf " \n"
+          printf "Downloading from Pike's GitHub instead...\n"
+          cd /tmp
+          rm -R ./AppleIntelInfo
+          git clone "${gPikerAII}"
+          if [[ $? -ne 0 ]];
+            then
+              printf 'An error occurred! Make sure your network connection is active and/or if you have installed Xcode properly!\n'
+              exit 1
+            else
+              cd AppleIntelInfo
+              xcodebuild > /dev/null
+              if [[ $? -eq 0 ]];
+                then
+                  mv ./build/Release/AppleIntelInfo.kext $HOME/Desktop
+                  gKext="$dkpKext"
+                else
+                  printf 'Uh-oh! Compilation failed!!!\n'
+                  exit 1
+              fi
+          fi
+      fi
     else
-    #
-    # unload kext and exit script
-    #
-    echo ''
-    sudo kextunload -b com.pikeralpha.driver.AppleIntelInfo
-    exit 0;
+      gKext="${gRepo}/${gAii}"
   fi
 }
 
-#===============================================================================##
-## POPULATE STATES #
-##==============================================================================##
-function populateStates {
-  let timesToLoop=10
-  let timeLeft=10
 
-  for (( i=0; i < $timesToLoop; i++ ))
-  do
-    sudo cat /tmp/AppleIntelInfo.dat
-    timeLeft="$((timeLeft - 1))"
-    if (( $timeLeft >= 1 ))
-    then
-      echo ''
-      echo '----------------------------------------------------'
-      echo "Status: Populating the CPU states ${timeLeft} more time(s)..."
-      echo '----------------------------------------------------'
-      echo ''
-      sleep 10
-    else
-      echo ''
-      echo '----------------------------------------------------'
-      echo "Status: Done populating the CPU states!"
-      echo '----------------------------------------------------'
-    fi
+#===============================================================================##
+## SET PERMISSIONS #
+##==============================================================================##
+
+function _set_perm()
+{
+  chown -R 0:0 "${gKext}"
+  chmod -R 755 "${gKext}"
+}
+
+
+#===============================================================================##
+## USER ABORTS SCRIPT #
+##==============================================================================##
+
+function _clean_up() {
+  printf "User aborted! Cleaning up script...\033[0K\r\n"
+  sudo kextunload "${gKext}"
+  clear
+}
+
+#===============================================================================##
+## TIMOUT KEXT AND OUTPUT #
+##==============================================================================##
+
+function showTimer()
+{
+  secs=$((3 * 60))
+  while [ $secs -gt 0 ]; do
+    printf " ----- Generating kext output in: $secs seconds -----\033[0K\r"
+    sleep 1
+    : $((secs--))
   done
 }
 
 #===============================================================================##
-## MAIN #
+## LOAD AND UNLOAD KEXT #
 ##==============================================================================##
-function main(){
-  sudo chown -R 0:0 ${gFile}
-  sudo chmod -R 755 ${gFile}
-  sudo kextload ${gFile}
-  echo ''
-  echo '----------------------------------------------------'
-  echo "Status: Populating the CPU states 10 times..."
-  echo '----------------------------------------------------'
-  sleep 3
-  populateStates
-  endScript 1
+
+function LoadAndUnload()
+{
+  sudo kextload "${gKext}"
+  if [[ $? == 0 ]];
+    then
+      printf " \n"
+      printf "${gAii} has been loaded! Sit back and relax. :-)\n"
+      printf " \n"
+      showTimer
+      clear
+      kextunload "${gKext}"
+    else
+      printf "ERROR loading ${gKext} !!!\n"
+      exit 1
+  fi
 }
 
-#===============================================================================##
-## CHECK IF FILE EXISTS #
-##==============================================================================##
-function checkFileExist() {
-  cd ~/Desktop
 
-  if [ ! -e ${gFile} ];
-  then
-    echo "${gFile} not found!"
-    sleep 1.5
-    endScript
-  else
-    echo "This script must be temporarily run as root!"
-    main
+#===============================================================================##
+## PRINT RESULT #
+##==============================================================================##
+
+
+function _printResult()
+{
+  if [ -f /tmp/AppleIntelInfo.dat ];
+    then
+      cat /tmp/AppleIntelInfo.dat
+      echo ''
+      echo '----------------------------------------------------'
+      echo "Done populating the CPU states!"
+      echo '----------------------------------------------------'
+      exit 0
+    else
+      printf 'ERROR populating AppleIntelInfo/s results, aborting...\n'
+      exit 1
   fi
 }
 
@@ -110,16 +198,35 @@ function checkFileExist() {
 ## GREET USER #
 ##==============================================================================##
 function greet(){
-  echo '     AppleIntelInfo Loader Version 0.0.2 - Copyright (c) 2017 by M.F.C.'
-  echo '--------------------------------------------------------------------------------'
+  echo '     AppleIntelInfo Loader Version 1.0.0 - Copyright (c) 2017 by M.F.C. and PMHeart'
+  echo '------------------------------------------------------------------------------------------'
   echo ''
   sleep 0.5
 }
 
+
 #===============================================================================##
-## START PROGRAM #
+## START #
 ##==============================================================================##
-clear
-greet
-checkFileExist
-#================================================================================
+
+
+function main()
+{
+  clear
+  greet
+  _getSIPStat
+  _find_kext
+  _set_perm
+  LoadAndUnload
+  _printResult
+}
+
+trap '{ _clean_up; exit 1; }' INT
+
+if [[ `id -u` -ne 0 ]];
+  then
+    echo "This script must be run as ROOT!"
+    sudo "$0"
+  else
+    main
+fi
